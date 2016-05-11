1aabd1596d4cf62cf0cb2edb252863e2e4104c3d
John Carty <john.carty@gmail.com>
2016-05-10 21:41:30 -0400

    Initial Commit

---
+++ Heap-Efficiency/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Heap-Efficiency
+//
+//  Created by John Carty on 5/10/16.
+//  Copyright © 2016 John Carty. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
